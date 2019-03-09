56c17d9d57c42b66ccd348fa3d072f9f7695d678
Bart Stassen <bart@lightcurve.io>
2019-03-09 13:35:25 +0100

    now.json
    now.json

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
 import initExpress from './initExpress'
 import initApi from './initApi'
-import initMailer from './initMailer'
+//import initMailer from './initMailer'
 
 const start = async (config) => {
   const app = await initExpress(config)
   await initApi(app, config)
-  const mailer = initMailer(config)
+  //const mailer = initMailer(config)
 
   const wd = require('./watchdog')(config)
   wd.start((diff, html) => {
     console.error('!!! result mismatched !!!')
     console.log(diff)
-    mailer.send(html)
+    //mailer.send(html)
   })
 }
 
